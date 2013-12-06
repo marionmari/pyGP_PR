7978f08b000a71e06b12897a85a0c8d11ca17f07
marionmari <marion.neumann@iais.fraunhofer.de>
2013-12-06 14:04:43 +0100

    Functional -> Procedural

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Functional Gaussian Processes'
+project = u'Procedural Gaussian Processes'
 copyright = u'2013, Marion Neumann, Daniel Marthaler, Shan Huang, Kristian Kersting'
 
 # The version info for the project you're documenting, acts as replacement for
